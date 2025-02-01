45388a525f3e4d7c74e26bf0d1a9324b958acf76
drews16 <109108583+drews16@users.noreply.github.com>
2025-02-01 21:59:48 +0400

    рефакторинг

--- services/course/src/Course.Application.Contracts/Courses/Dto/CourseListDto.cs
+++ services/course/src/Course.Application.Contracts/Courses/Dto/CourseListDto.cs
@@ -4,6 +4,6 @@
 	{
 		public required Guid Id { get; init; }
 		public required string Title { get; init; }
-		public string? Description { get; init; }
+		public required string? Description { get; init; }
 	}
 }
